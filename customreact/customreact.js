1293f45773d4b05aa5179938fefeb79bc8b863e3
anmol3538 <anmolaggarwal557@gmail.com>
2024-10-07 22:05:00 +0530

    commit

--- customreact/customreact.js
+++ customreact/customreact.js
@@ -1,10 +1,24 @@
 function customrender(reactElement, maincontainer){
+    // const domElement = document.createElement(reactElement.type)
+    // domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href', reactElement.props.href)
+    // domElement.setAttribute('target', reactElement.props.target)
+    // maincontainer.appendChild(domElement)
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
-    domElement.setAttribute('href', reactElement.props.href)
-    domElement.setAttribute('target', reactElement.props.target)
+    for(const prop in reactElement.props){
+        if(prop==='children') continue
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
     maincontainer.appendChild(domElement)
 }
-
+const reactElement = {
+    type: 'a',
+    props:{
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
 const maincontainer = document.querySelector('#root');
-
+customrender(reactElement, maincontainer)
